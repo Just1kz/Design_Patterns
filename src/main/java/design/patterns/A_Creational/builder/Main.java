f1a3be0d57a00636053c3d6f22f6e367cc345043
Just1kz <exelent.pride@rambler.ru>
2022-04-21 19:02:34 +0700

    описание к прототипу и пагинационный вывод элементов в локальном контейнере

--- src/main/java/design/patterns/A_Creational/builder/Main.java
+++ src/main/java/design/patterns/A_Creational/builder/Main.java
@@ -4,6 +4,12 @@ import design.patterns.A_Creational.builder.example_one.CarBuilderOne;
 import design.patterns.A_Creational.builder.example_two.CarBuilderTwo;
 import design.patterns.A_Creational.builder.example_two.Director;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
 public class Main {
     public static void main(String[] args) {
         //Example_one
@@ -24,5 +30,37 @@ public class Main {
         car = carBuilderTwo.getResult();
         System.out.println(car);
         System.out.println(carBuilderTwo);
+
+        List<Integer> list = new ArrayList<>(100);
+        for(int x=0; x<100; x++) {
+            list.add(x);
+        }
+        System.out.println(list.size());
+
+        int pageSize = 45;
+        int pageNumber = 3;
+        int start = (pageNumber - 1) * pageSize;
+        int end = Math.min((start + pageSize), list.size());
+        System.out.println(IntStream.range(start, end).mapToObj(list::get).collect(Collectors.toList()));
+
+        Car car1 = new Car("as", Car.Transmission.MANUAL, 100);
+        Car car2 = new Car("as", Car.Transmission.MANUAL, 100);
+        Car car3 = new Car("as", Car.Transmission.MANUAL, 100);
+        Car car4 = new Car("as", Car.Transmission.MANUAL, 150);
+        Car car5 = new Car("as", Car.Transmission.MANUAL, 150);
+        Car car6 = new Car("as", Car.Transmission.MANUAL, 200);
+        List<Car> list2 = new ArrayList<>();
+        list2.add(car1);
+        list2.add(car2);
+        list2.add(car3);
+        list2.add(car4);
+        list2.add(car5);
+        list2.add(car6);
+        Map<String, Long> map = list2.stream()
+                .collect(Collectors.groupingBy((x->String.valueOf(x.getSpeed())), Collectors.counting()));
+        System.out.println(map);
+        long all = map.values().stream().mapToLong(x->x).sum();
+        map.put("all", all);
+        System.out.println(map);
     }
 }
